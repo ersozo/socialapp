5dee3a2cf54b9cda94014c8b1324a8b6ef3a9da8
ozgur <ozgur.ersoz3@gmail.com>
2021-02-15 18:11:00 +0300

    initial

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Projem',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: AnaSayfa("benim sayfam"),
+    );
+  }
+}
+
+class AnaSayfa extends StatelessWidget {
+
+  final String name;
+
+  AnaSayfa(this.name);
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(name),
+      ),
+    );
+  }
+}
